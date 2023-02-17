674264a9f81ac805cf0818d60bc4b67e268dbd14
Michel Geoffroy <geoffroy827@gmail.com>
2023-02-17 02:00:08 +0100

    modified file

--- sources/fridge.py
+++ sources/fridge.py
@@ -35,7 +35,6 @@ class Fridge:
             default_sauce = 0
         self._ingredients: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, default_ingredients)
         self._sauces: Dict[str, int] = dict.fromkeys(ALL_SAUCES, default_sauce)
-    def __init__(self, default: int = 0):
         self._count: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, 0)
 
     @property
